3c8bedbb10e0add694d7cedd2d88439989df336b
Dominika <dom.kadleckova@gmail.com>
2024-11-11 17:47:18 +0100

    finished?

--- substring.rb
+++ substring.rb
@@ -1,9 +1,14 @@
-def substrings(word, dictionary)
-  count = 0
-  dictionary.each {|dict_word| count += 1 if dict_word.downcase == word.downcase}
-  hash = {word => count}
-  puts(hash)
+def substrings(sentence, dictionary)
+  hash = Hash.new
+  sentence.split(" ").each {|word|
+    count = 0
+    dictionary.each {|dict_word| count += 1 if word.downcase.include?(dict_word.downcase)}
+    hash[word] = count
+  }
+  puts(hash.select{|key, data| key if data > 0})
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("", dictionary)
+substrings("low", dictionary)
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+
