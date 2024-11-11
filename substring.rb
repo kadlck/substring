b8f3bc7e41c6e1dec11894513f3037b204f8e10c
Dominika <dom.kadleckova@gmail.com>
2024-11-11 18:26:26 +0100

    Does the thing

--- substring.rb
+++ substring.rb
@@ -1,14 +1,14 @@
 def substrings(sentence, dictionary)
   hash = Hash.new
-  sentence.split(" ").each {|word|
+  dictionary.each {|dict_word|
     count = 0
-    dictionary.each {|dict_word| count += 1 if word.downcase.include?(dict_word.downcase)}
-    hash[word] = count
+    sentence.split(" ").each {|word| count += 1 if word.downcase.include?(dict_word.downcase)}
+    hash[dict_word] = count
   }
   puts(hash.select{|key, data| key if data > 0})
 end
 
 dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
-substrings("low", dictionary)
+substrings("below", dictionary)
 substrings("Howdy partner, sit down! How's it going?", dictionary)
 
